09014d9e5478bdc7d56ccd8c8b4db28b4e4be82c
Gabriel Telles <telleswq@gmail.com>
2026-03-16 19:42:32 -0300

    adicionando schema de mensagem

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title='Minha API BALA wq')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
